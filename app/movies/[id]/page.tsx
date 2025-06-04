ddc23f9c5c146a302e4ad00c639bbe64774a3c00
Tugsibility <akimatsuchohideaki@gmail.com>
2025-06-04 13:07:28 +0800

    added casts

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,7 +1,8 @@
 import MovieCard from "@/components/movie/MovieCard";
 import Badge from "@/components/common/Badge";
 import StaffInfo from "@/components/common/StaffInfo";
-import { Divide, Star } from "lucide-react";
+import { Star, ChevronRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const TMDB_ACCESS_TOKEN =
@@ -30,16 +31,6 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   const castdata = await cast.json();
 
-  // const name = castdata.crew
-  //   .slice(0, 5)
-  //   .map((person) => console.log(person.name));
-  // console.log(name);
-
-  // const directing = castdata.crew.map((person) =>
-  //   console.log(person.known_for_department.filter())
-  // );
-  // console.log(directing);
-
   console.log(castdata);
 
   const similar = await fetch(
@@ -55,82 +46,84 @@ export default async function Page({ params }: { params: { id: string } }) {
   console.log(similardata);
 
   return (
-    <div className="w-[1080px] flex flex-col gap-8">
-      <div className="flex justify-between text-[#09090B]">
-        <div>
-          <h1 className="text-4xl font-bold leading-10">{movie.title}</h1>
-          <p className="text-lg font-normal">
-            {movie.release_date} · {movie.adult ? "18+" : "PG"} ·{" "}
-            {movie.runtime} mins
-          </p>
-        </div>
-        <div>
-          <p className="text-xs font-medium">Rating</p>
-          <div className="flex">
-            <Star className="w-7 h-7 fill-[#FDE047] text-[#FDE047]" />
-            <div>
-              <p className="text-lg font-semibold">
-                {movie.vote_average?.toFixed(1)}
-                <span className="text-base font-normal text-[#71717A]">
-                  /10
-                </span>
-              </p>
-
-              <p className="text-[#71717] text-base font-normal">37k</p>
+    <section className="flex justify-center">
+      <div className="w-[1080px] flex flex-col gap-8">
+        <div className="flex justify-between text-[#09090B]">
+          <div>
+            <h1 className="text-4xl font-bold leading-10">{movie.title}</h1>
+            <p className="text-lg font-normal">
+              {movie.release_date} · {movie.adult ? "18+" : "PG"} ·{" "}
+              {movie.runtime} mins
+            </p>
+          </div>
+          <div>
+            <p className="text-xs font-medium">Rating</p>
+            <div className="flex">
+              <Star className="w-7 h-7 fill-[#FDE047] text-[#FDE047]" />
+              <div>
+                <p className="text-lg font-semibold">
+                  {movie.vote_average?.toFixed(1)}
+                  <span className="text-base font-normal text-[#71717A]">
+                    /10
+                  </span>
+                </p>
+                <p className="text-[#71717A] text-base font-normal">37k</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="flex gap-8 h-[428px]">
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt="small"
-          className="w-[290px] rounded-sm"
-        />
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-          alt="big pic"
-          className="w-full rounded-sm"
-        />
-      </div>
-      <div className="flex flex-col gap-5">
-        <div className="flex gap-3">
-          {movie.genres?.map((genre) => (
-            <Badge key={genre.id} genre={genre.name} />
-          ))}
-        </div>
-
-        <div className="text-base font-normal text-[#09090B]">
-          {movie.overview}
+        <div className="flex gap-8 h-[428px]">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt="small"
+            className="w-[290px] rounded-sm"
+          />
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+            alt="big pic"
+            className="w-full rounded-sm"
+          />
         </div>
         <div className="flex flex-col gap-5">
-          {/* {castdata?.crew?.map(
-            (cast: { job: string; original_name: string }) => {
-              if (cast.job === "Director") {
-                return <span className="px-2 ">{cast.original_name}</span>;
-              }
-            }
-          )} */}
-          <div className="w-full">
-            <div className="flex text-[#09090B] gap-[53px] text-base pb-1 ">
-              <h3 className="font-bold">Director</h3>
-              <p className="font-normal">
-                Winnie Holzman · Dana Fox · Gregory Maguire
-              </p>
-            </div>
+          <div className="flex gap-3">
+            {movie.genres?.map((genre) => (
+              <Badge key={genre.id} genre={genre.name} />
+            ))}
+          </div>
+          <div className="text-base font-normal text-[#09090B]">
+            {movie.overview}
+          </div>
+          <div className="flex flex-col gap-5">
+            <StaffInfo job="Director" role="Director" castdata={castdata} />
+            <StaffInfo job="Writer" role="Writers" castdata={castdata} />
+            <StaffInfo role="Stars" castdata={castdata} type="cast" />
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between pb-2">
+            <p className="text-xl md:text-2xl font-semibold leading-8">
+              More like this
+            </p>
+            <Button
+              variant="ghost"
+              className="flex items-center gap-2 text-[#09090B]"
+            >
+              See more
+              <ChevronRight className="w-4 h-4" />
+            </Button>
+          </div>
+          <div className="flex gap-8">
+            {similardata?.results?.slice(0, 5).map((sim) => (
+              <MovieCard
+                key={sim.id}
+                image={`https://image.tmdb.org/t/p/w300${sim.poster_path}`}
+                rating={sim.vote_average}
+                title={sim.title}
+              />
+            ))}
           </div>
         </div>
       </div>
-      <div className="flex gap-8">
-        {similardata?.results?.slice(0, 5).map((sim) => (
-          <MovieCard
-            key={sim.id}
-            image={`https://image.tmdb.org/t/p/w300${sim.poster_path}`}
-            rating={sim.vote_average}
-            title={sim.title}
-          />
-        ))}
-      </div>
-    </div>
+    </section>
   );
 }
--- components/common/StaffInfo.tsx
+++ components/common/StaffInfo.tsx
@@ -1,12 +1,37 @@
-const StaffInfo = ({ name, role }: { name: string; role: string }) => {
+import { Separator } from "@/components/ui/separator";
+const StaffInfo = ({
+  job,
+  role,
+  castdata,
+  type = "crew",
+}: {
+  job?: string;
+  role: string;
+  castdata: any;
+  type?: "crew" | "cast";
+}) => {
+  let staff;
+
+  if (type === "cast") {
+    staff = castdata?.cast?.slice(0, 3);
+  } else {
+    staff = castdata?.crew?.filter((person: any) => person.job === job);
+  }
+
   return (
     <div className="w-full">
-      <div className="flex text-[#09090B] gap-[53px] text-base pb-1 ">
-        <h3 className="font-bold">Director</h3>
+      <div className="flex text-[#09090B] gap-[53px] text-base pb-1">
+        <h3 className="font-bold">{role}</h3>
         <p className="font-normal">
-          Winnie Holzman · Dana Fox · Gregory Maguire
+          {staff?.map((person: any, index: number) => (
+            <span key={index}>
+              {person.name}
+              {index < staff.length - 1 && " · "}
+            </span>
+          ))}
         </p>
       </div>
+      <Separator />
     </div>
   );
 };
