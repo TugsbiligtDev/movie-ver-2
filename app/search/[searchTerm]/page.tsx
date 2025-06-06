bd7d03a858cc0d3e9b28484216c3dd4eb3098aea
Tugsbiligt <155721097+TugsbiligtDev@users.noreply.github.com>
2025-06-06 16:59:04 +0800

    Update page.tsx

--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -1,11 +1,18 @@
 
-export default async function searchPage({params})  {
-    const searchTerm= params.searchTerm
-    const res= await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${bbf18a55b60c1ac8ab73d875ce112616}query=${searchTerm}&language=en-US&page=1&include+adult=false`)
+export default async function SearchPage({params}: {params: {searchTerm: string}}) {
+    const searchTerm = params.searchTerm
+    const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
+    )
     const data = await res.json()
-    const results= data.results
-  return (
-    <div>page</div>
-  )
+    const results = data.results
+    
+    return (
+        <div>page</div>
+    )
 }
+
+
+TMDB_API_KEY=bbf18a55b60c1ac8ab73d875ce112616
+
 
